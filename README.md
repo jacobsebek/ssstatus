640020a9155052fde9412151f7ca58644ecb6042
Jakub Sebek <jamertwo@gmail.com>
2021-08-09 04:16:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# ssstatus
+ssstatus (**s**uper **s**imple status) is a bloat-free status line generator for i3bar. It minimises CPU time and is customisable with its own simple configuration format. ssstatus does nothing more and nothing less than it needs to do, it is currently standing at ~150 lines of C99 code.
+
+## Compiling
+ssstatus doesn't have any dependencies, you can trivially compile it with any compiler conforming to C99, POSIX.1, and the 4.4BSD `strsep` function
+
+## Usage
+ssstatus reads its configuration from the standard input. You can use it as any other status generator for i3bar, simply specify the invoking command in the status_command option of the bar block in your i3 configuration file.
+```
+bar {
+    status_command ssstatus < ~/.ssstatus
+}
+```
+```
+status_command echo "date +%T;1s;;" | ssstatus
+```
+
+## Configuration
+ssstatus can be configured using its its minimal configuration format.
+This format is designed to be easy to read, parse and not rely on newline characters.  
+Each status entry is made up of fields separated by semicolons (`;`) and is terminated by double semicolons (`;;`).
+
+The first field is the shell command whose first line of the standard output is to be displayed in the status. 
+
+The second field specifies either the update interval of the entry or the POSIX real-time signal only upon which the entry be updated. This entry is made up of a base-10 integer immediately followed by `s`(seconds), `m`(minutes), `h`(hours), `!`(The real-time signal index, the actual signal number will be `SIGRTMIN`+ the specified index).
+
+The third field is optional and specifies the text colour of the entry as a hexadecimal integer.
+
+Comments can be inserted by enclosing them with `#` on both sides. Comments can only appear in between entries.
+
+Follows an example of a configuration file:
+```
+# Date and time, every 5 seconds, default colour #
+date +"%D %T"; 5s;;
+# Current weather, hourly, purple #
+curl -Ss wttr.in?format="%C+%f"; 1h; 0xD0D0FF;;
+# Current keyboard layout, upon SIGRTMIN+8, green 
+  `pkill -$((65+8)) ssstatus` can be used to update on FreeBSD#
+setxkbmap -query | awk '/layout/{print $2}'; 8!; d0ffd0;;
+```
+The following status is produced:
+![A screenshot of the resulting status bar](screen1.png)
---
+++ status.c
@@ -0,0 +1,146 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <signal.h>
+#include <time.h>
+#include <limits.h>
+
+#define ENTRY_BUF_SIZE 128
+
+struct entry {
+    char* command;
+    int trigger;
+    unsigned long color;
+
+    time_t next;
+    char buf[ENTRY_BUF_SIZE];
+};
+
+struct entry* load(int* num, sigset_t* sigset) {
+
+    char* buf = NULL;
+    {
+        int bufsize = 1, pos = 0, numread;
+        for (;buf = realloc(buf, (bufsize+=128)), numread = fread(buf+pos, 1, 128, stdin);
+              pos+=numread);
+         buf[pos] = '\0';
+    }
+
+    struct entry* entries = NULL;
+    *num = 0;
+    sigemptyset(sigset);
+
+    for (char* c = buf; *c; c++) {
+        for (; isspace(*c); c++); 
+        if (*c == '\0') break;
+
+        if (*c == '#') {
+            c = strchr(c+1, '#')+1;
+            continue;
+        }
+
+        entries = realloc(entries, (++*num) * sizeof(struct entry));
+        struct entry* entry = &entries[*num-1];
+
+        entry->command = strdup(strsep(&c, ";"));
+
+        char* end;
+        entry->trigger = strtoul(strsep(&c, ";"), &end, 10);
+        switch (*end) {
+            case 'm': entry->trigger *= 60;             break;
+            case 'h': entry->trigger *= 3600;           break;
+            case '!': 
+                sigaddset(sigset, SIGRTMIN+entry->trigger);
+                entry->trigger = -entry->trigger; 
+            break;
+        }
+
+        if (*c != ';') {
+            entry->color = strtoul(c, NULL, 16);
+            strsep(&c, ";");
+        } else
+            entry->color = 0xFFFFFF;
+    }
+
+    free(buf);
+    return entries;
+}
+
+static void fetch(struct entry* entry) {
+    entry->buf[0] = '\0';
+
+    FILE* out = popen(entry->command, "r");
+    if (!out) goto fail;
+    fgets(entry->buf, ENTRY_BUF_SIZE, out);
+    if (pclose(out) != 0) goto fail;
+
+    strtok(entry->buf, "\n");
+    return;
+fail:
+    strncpy(entry->buf, "Command failed", ENTRY_BUF_SIZE);
+}
+
+static void flush(struct entry* entries, int num_entries) {
+    putchar('[');
+    for (int i = 0; i < num_entries; i++) {
+        putchar('{');
+
+        printf("\"full_text\":\"%s\"", entries[i].buf);
+        printf(",\"color\":\"#%lx\"", entries[i].color);
+
+        putchar('}');
+        if (i < num_entries-1)
+            putchar(',');
+    }
+    puts("],");
+    fflush(stdout);
+}
+
+int main(int argc, char *argv[]) {
+
+    int num_entries;
+    sigset_t rtsigset;
+    struct entry* entries = load(&num_entries, &rtsigset);
+
+    puts("{\"version\": 1 }");
+    putchar('[');
+
+    for (int i = 0; i < num_entries; i++) {
+        fetch(&entries[i]);    
+        entries[i].next = time(NULL) + entries[i].trigger;
+    }
+    flush(entries, num_entries);
+
+    while (1) {
+        time_t timest = time(NULL);
+
+        time_t wait = INT_MAX;
+        for (int i = 0; i < num_entries; i++) {
+            if (entries[i].trigger <= 0) continue;
+
+            if (timest >= entries[i].next) wait = 0;
+            else if (entries[i].next - timest < wait)
+                wait = entries[i].next - timest;
+        }
+
+        struct timespec wait_ts = {.tv_sec = wait};
+        int sig = sigtimedwait(&rtsigset, NULL, &wait_ts);
+        if (sig > 0)
+            for (int i = 0; i < num_entries; i++)
+                if (sig-SIGRTMIN == -entries[i].trigger)
+                    fetch(&entries[i]);
+
+        for (int i = 0; i < num_entries; i++) {
+            if (entries[i].trigger <= 0) continue;
+
+            timest = time(NULL);
+            if (timest >= entries[i].next) {
+                fetch(&entries[i]);
+                entries[i].next = timest + entries[i].trigger;
+            }
+        }
+
+        flush(entries, num_entries);
+    }
+}
